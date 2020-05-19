c56185b3e089c1319f0baacf0c80a147add295a7
Kairsten Fay <kairsten.fay@gmail.com>
2020-05-19 10:52:12 -0700

    utils: Refactor hierarchy creation
    Move the hierarchy dataframe creation logic into a new utils file for
    future reuse by `transform`.

---
+++ lib/utils/hierarchy_dataframe.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+def hierarchy_dataframe(metadata: pd.DataFrame, hierarchy_columns: list) -> pd.DataFrame:
+    """
+    Given a ``pd.DataFrame`` `metadata`, returns a `pd.DataFrame` with one row
+    for every unique location hierarchy defined by the `hierarchy_columns` (e.g.
+    region, country, and division). Looks for location columns matching the
+    original resolution name (e.g. 'region') and any stubname of it (e.g.
+    'region_exposure').
+    """
+    metadata = metadata.rename(columns={
+        resolution: f'{resolution}_strain' for resolution in hierarchy_columns
+    })
+
+    return pd \
+        .wide_to_long(metadata,
+            stubnames=hierarchy_columns,
+            i=['gisaid_epi_isl'],
+            j="resolution_type",
+            sep='_',
+            suffix='\w+') \
+        .reset_index()[hierarchy_columns] \
+        .drop_duplicates() \
+        .reset_index(drop=True) \
+        .sort_values(by=hierarchy_columns)
